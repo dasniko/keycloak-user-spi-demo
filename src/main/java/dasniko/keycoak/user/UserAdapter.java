8f24d601a6cdc1bd39b1a8d8628f106969966aca
Niko Köbler <niko@n-k.de>
2020-09-24 23:26:42 +0200

    update to KC 11.x and adjust to new internal user storage spi handling, solves #5

--- src/main/java/dasniko/keycoak/user/UserAdapter.java
+++ src/main/java/dasniko/keycoak/user/UserAdapter.java
@@ -12,17 +12,21 @@ import org.keycloak.storage.adapter.AbstractUserAdapterFederatedStorage;
 public class UserAdapter extends AbstractUserAdapterFederatedStorage {
 
     private final DemoUser user;
-    private final String keycloakId;
 
     public UserAdapter(KeycloakSession session, RealmModel realm, ComponentModel model, DemoUser user) {
         super(session, realm, model);
         this.user = user;
-        this.keycloakId = StorageId.keycloakId(model, user.getId());
+        setFirstName(user.getFirstName());
+        setLastName(user.getLastName());
+        setEmail(user.getEmail());
     }
 
     @Override
     public String getId() {
-        return keycloakId;
+        if (storageId == null) {
+            storageId = new StorageId(storageProviderModel.getId(), user.getId());
+        }
+        return storageId.getId();
     }
 
     @Override
@@ -33,35 +37,5 @@ public class UserAdapter extends AbstractUserAdapterFederatedStorage {
     @Override
     public void setUsername(String username) {
         user.setUsername(username);
-    }
-
-    @Override
-    public String getEmail() {
-        return user.getEmail();
-    }
-
-    @Override
-    public void setEmail(String email) {
-        user.setEmail(email);
-    }
-
-    @Override
-    public String getFirstName() {
-        return user.getFirstName();
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        user.setFirstName(firstName);
-    }
-
-    @Override
-    public String getLastName() {
-        return user.getLastName();
-    }
-
-    @Override
-    public void setLastName(String lastName) {
-        user.setLastName(lastName);
     }
 }
